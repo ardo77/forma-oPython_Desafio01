7ff573e833e08f1c60c6c75c8a51f3daaf39a4be
Anderson Oliveira <anderson.oliveira@valmont.com>
2025-03-12 13:30:32 -0300

    Primeiro commit

---
+++ agenda.py
@@ -0,0 +1,11 @@
+agenda = []
+
+while True:
+    print("\n__Menu da Agenda__")
+    print("1. Adicionar um Contato")
+    print("2. Visualizar Contatos")
+    print("3. Editar um Contato")
+    print("4. Marcar ou Desmarcar um Contato com Favorito")
+    print("5. Lista de Contatos Favoritos")
+    print("6. Apagar um Contato")
+    print("7. Sair")
